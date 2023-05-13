6431f45001449e696136a9e926072fc3e148f7d5
Milhan Osman <milhanomarosman@gmail.com>
2023-05-13 12:04:03 +0300

    component of cal

---
+++ src/components/CalCard.jsx
@@ -0,0 +1,32 @@
+// import React from 'react'
+
+const CalCard = () => {
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="prev-operand"></div>
+        <div className="current-operand"></div>
+      </div>
+      <button className="span-2">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-2">=</button>
+    </div>
+  );
+};
+
+export default CalCard;
